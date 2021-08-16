cc40f1856ad03bc671ac2f1174a0f2ce7a3fc277
Sergei Zharinov <zharinov@users.noreply.github.com>
2021-08-16 14:37:30 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+module.exports = {
+  rules: {
+    'jest-root-describe': {
+      create(context) {
+        const absoluteFileName = context.getFilename();
+        if (!absoluteFileName.endsWith('.spec.ts')) {
+          return {};
+        }
+        const relativeFileName = absoluteFileName
+          .replace(process.cwd(), '')
+          .replace(/^\/(?:lib|src|test)\//, '');
+        const testName = String(relativeFileName.replace(/\.spec\.ts$/, ''));
+        return {
+          CallExpression(node) {
+            const { callee } = node;
+            if (
+              callee.type === 'Identifier' &&
+              callee.name === 'describe' &&
+              node.parent.parent.type === 'Program'
+            ) {
+              const [descr] = node.arguments;
+              const isOkay =
+                descr.type === 'Literal' &&
+                typeof descr.value === 'string' &&
+                testName === descr.value;
+              if (!isOkay) {
+                context.report({
+                  node: descr,
+                  message: `Test must be described by this string: '${testName}'`,
+                  fix(fixer) {
+                    return fixer.replaceText(descr, `'${testName}'`);
+                  },
+                });
+              }
+            }
+          },
+        };
+      },
+    },
+  },
+};
